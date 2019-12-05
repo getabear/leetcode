e272011e20f314384e5346f69bd48885d018348a
getabear <1874178998@qq.com>
2019-12-05 14:10:57 +0800

    加了一种大佬解法

--- 合并两个有序链表.py
+++ 合并两个有序链表.py
@@ -34,3 +34,17 @@ class Solution:
             return None
         bef.next=None
         return ret
+
+    def mergeTwoLists2(self, l1: ListNode, l2: ListNode) -> ListNode:
+        #方法二:递归解法   看了大佬的作品
+        if l1==None:
+            return l2
+        if l2==None:
+            return l1
+        else :
+            if (l1.val<l2.val):
+                l1.next=self.mergeTwoLists2(l1.next,l2)
+                return l1
+            else :
+                l2.next=self.mergeTwoLists2(l1,l2.next)
+                return l2
