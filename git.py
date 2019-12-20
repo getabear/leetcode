a1d9838d459481a4afd194001f576f47e3ca3f8c
getabear <1874178998@qq.com>
2019-12-20 20:16:22 +0800

    构造链表用的,适合我本地调试

--- git.py
+++ git.py
@@ -1,2 +1,12 @@
 # 这是一个测试
-print("success")
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+def creat(nums):   #构造一个链表的函数
+    ret=ListNode(-1)
+    res=ret
+    for i in nums:
+        ret.next=ListNode(i)
+        ret=ret.next
+    return res.next
