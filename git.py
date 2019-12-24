4e28c6b1a094edda717e9b0035f162bc8c5d763c
getabear <1874178998@qq.com>
2019-12-24 11:12:02 +0800

    常用的一些构造的数据结构的函数

--- git.py
+++ git.py
@@ -3,6 +3,7 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
 def creat(nums):   #构造一个链表的函数
     ret=ListNode(-1)
     res=ret
@@ -15,3 +16,37 @@ def display(head):   #展示一个链表的值
     while head:
         print(head.val)
         head=head.next
+
+
+
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+def creat_er(nums):
+    def fun(nums,node):
+        if len(nums)==0 or node==None:
+            return None
+        if  len(nums)>0:
+            if nums[0]!=None:
+                node.left=TreeNode(nums[0])
+        if len(nums)>1:
+            if nums[1]!=None:
+                node.right=TreeNode(nums[1])
+        fun(nums[2:],node.left)
+        fun(nums[4:],node.right)
+
+    if len(nums)<1:
+        return None
+    node=TreeNode(nums[0])
+    fun(nums[1:],node)
+    return node
+
+nums=[3,9,20,None,None,15,7,8,9]
+node=creat_er(nums)
+print(node)
+
