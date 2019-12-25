70699e71994e32efd1ffe66fb760b4e40e135d02
getabear <1874178998@qq.com>
2019-12-25 20:18:13 +0800

    .

--- git.py
+++ git.py
@@ -27,26 +27,8 @@ class TreeNode:
         self.left = None
         self.right = None
 
-def creat_er(nums):
-    def fun(nums,node):
-        if len(nums)==0 or node==None:
-            return None
-        if  len(nums)>0:
-            if nums[0]!=None:
-                node.left=TreeNode(nums[0])
-        if len(nums)>1:
-            if nums[1]!=None:
-                node.right=TreeNode(nums[1])
-        fun(nums[2:],node.left)
-        fun(nums[4:],node.right)
 
-    if len(nums)<1:
-        return None
-    node=TreeNode(nums[0])
-    fun(nums[1:],node)
-    return node
-
-nums=[3,9,20,None,None,15,7,8,9]
-node=creat_er(nums)
-print(node)
+# nums=[3,9,20,None,None,15,7,8,9]
+# node=creat_er(nums)
+# print(node)
 
