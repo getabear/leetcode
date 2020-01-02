1075ae5a09bb7b6b696f948467d12500068cd929
getabear <1874178998@qq.com>
2020-01-02 14:14:38 +0800

    递归查找最大路径的问题

---
+++ 二叉树中的最大路径和.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def maxPathSum(self, root: TreeNode) -> int:
+        self.ret=float("-inf")
+        def fun(root: TreeNode):  #递归,返回左右树到上一个节点的最大值
+            if not root:
+                return 0
+            left=fun(root.left)
+            right=fun(root.right)
+            self.ret=max(left+right+root.val,self.ret)   #记录最大值
+            return max(0,left+root.val,right+root.val)  #返回左右树到上一个节点的最大值
+        fun(root)
+        return self.ret
