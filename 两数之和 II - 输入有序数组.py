0910d438d2c9a06e52a86f9186e67f6c13cd3942
getabear <1874178998@qq.com>
2020-01-07 17:49:34 +0800

    双指针逼近,算法复杂度O(n)

---
+++ 两数之和 II - 输入有序数组.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        index1=0
+        size=len(numbers)
+        index2=size-1
+        if size<2:
+            return
+        while index1<index2:
+            tp=numbers[index2]+numbers[index1]
+            if tp==target:
+                return [index1+1,index2+1]
+            elif tp>target:
+                index2-=1
+            else:
+                index1+=1
+        return
+
+a=Solution()
+numbers = [2, 7, 11, 15]
+target = 9
+print(a.twoSum(numbers,target))
+
+
