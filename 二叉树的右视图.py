2edc2ff1b60e9d3271f7524ce72768b532bfbd81
getabear <1874178998@qq.com>
2020-02-19 14:05:16 +0800

    二叉树的层次遍历,用的广度优先搜索

--- 二叉树的右视图.py
+++ 二叉树的右视图.py
@@ -9,7 +9,7 @@ class TreeNode:
 # deque
 # 数据类型来自于
 # collections 模块，支持从头和尾部的常数时间append/pop 操作。若使用 Python 的 list，
-# 通过 list.pop(0) 去除头部会消耗 O(n)O(n) 的时间。
+# 通过 list.pop(0) 去除头部会消耗 O(n) 的时间。
 #以后可以使用deque
 class Solution:
     def rightSideView(self, root: TreeNode) -> List[int]:
