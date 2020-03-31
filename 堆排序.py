aee4a591b2012ee0b3af434c926e73e416cb441f
getabear <1874178998@qq.com>
2020-03-31 15:31:29 +0800

    改进的堆排序

--- 堆排序.py
+++ 堆排序.py
@@ -14,16 +14,15 @@ class HeapSort:
             i*=2        #交换值后,判断子节点的值是否为子子节点中的最大
     def heapsort(self,arr):
         length=len(arr)
-        for i in range(length//2,-1,-1):#i 为每个非叶子节点
+        for i in range(length//2,0,-1):#i 为每个非叶子节点
             self.build(arr,i,length)
-        for i in range(length-1,1,-1):#传入的最小长度为4
-            arr[0],arr[i]=arr[i],arr[0]   #把最大值换到末尾
-            self.build(arr,0,i)    #数组的长度减一
-        arr[0], arr[1] = arr[1], arr[0]    #传入的长度为1的话会出现死循环,因此咱们传入最小长度为2,
-                                            # 最后自己手动交换0,1的位置完成最后的排序
+        for i in range(length-1,0,-1):#传入的最小长度为4
+            arr[1],arr[i]=arr[i],arr[1]   #把最大值换到末尾
+            self.build(arr,1,i)    #数组的长度减一
+        return arr[1:]
 
 a=HeapSort()
 arr=[3,4,5,1,4,6,72,4,5]
 print(arr)
-a.heapsort(arr)
-print(arr)
+print(a.heapsort([1]+arr))
+
