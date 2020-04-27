2149ad51f183aa557a987618522bbdc388f400e5
getabear <1874178998@qq.com>
2020-04-27 16:39:08 +0800

    程序的流程一定要拿捏好

--- 基本计算器 II.py
+++ 基本计算器 II.py
@@ -1,5 +1,6 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        s=s+'+'   #简化代码，不然后面老退出循环还需要入栈
         stack = []
         sign, tmp = '+', 0     #这里一定要牢记，这个sign是数字前面的符号！！！！重要！！！！
         for i in s:
@@ -23,19 +24,6 @@ class Solution:
                         stack.append(pre // tmp)
                 tmp = 0
                 sign = i
-        if sign == '+':
-            stack.append(tmp)
-        elif sign == '-':
-            stack.append(-tmp)
-        elif sign == '*':
-            pre = stack.pop(-1)
-            stack.append(pre * tmp)
-        else:
-            pre = stack.pop(-1)
-            if pre < 0:
-                stack.append(-pre // tmp * (-1))
-            else:
-                stack.append(pre // tmp)
         return sum(stack)
 
 
