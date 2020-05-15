b53056306dc74c6961564e31961350032816d5e6
getabear <1874178998@qq.com>
2020-05-15 16:48:20 +0800

    优化

--- git.py
+++ git.py
@@ -52,6 +52,31 @@ class er:
                 queue.append(newroot.right)
 
         return root
+def creat_er(nums):  #宽度优先遍历利用数组构造二叉树,nums是构造二叉树的数组
+    if not nums:
+        return
+    root=TreeNode(nums[0])
+    queue=[root]
+    length=len(nums)
+    index=1
+    while(queue):
+        newroot=queue.pop(0)
+        if index<length and newroot:
+            if nums[index]!=None:
+                newroot.left=TreeNode(nums[index])
+            else:
+                newroot.left=None
+            index+=1
+            queue.append(newroot.left)
+        if index<length and newroot:
+            if nums[index]!=None:
+                newroot.right=TreeNode(nums[index])
+            else:
+                newroot.right=None
+            index+=1
+            queue.append(newroot.right)
+
+    return root
 
 
 
