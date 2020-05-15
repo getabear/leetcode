12220e7ccc6aeadf73df311e41bb84eed01a96c9
getabear <1874178998@qq.com>
2020-05-15 16:47:48 +0800

    规律

---
+++ 下一个排列.py
@@ -0,0 +1,20 @@
+import bisect
+from typing import List
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        index = len(nums) - 1
+        while index > 0:
+            if nums[index] > nums[index - 1]:
+                break
+            index -= 1
+
+        if index > 0:
+            nums[index:] = sorted(nums[index:])
+            swap_index = bisect.bisect(nums, nums[index - 1], lo=index)
+            nums[swap_index], nums[index - 1] = nums[index - 1], nums[swap_index]
+        else:
+            nums.reverse()
