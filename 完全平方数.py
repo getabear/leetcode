ff98cd706999c6b63d8ccae4b7f4a33fd6431042
getabear <1874178998@qq.com>
2020-05-19 12:20:18 +0800

    动态规划

--- 完全平方数.py
+++ 完全平方数.py
@@ -1,15 +1,31 @@
-def fun(n):
-    min=1000
-    if n==0:
-        return 0
-    for i in range(1,100):
-        if(n-i*i>=0):
-            times=1+fun(n-i*i)
-        else:
-            break
-        if times<min:
-            min=times
-    return min
+class Solution1:
+    def numSquares(self, n: int) -> int:
 
-for n in range(20):
-    print(fun(n))
+        self.res=n
+        def fun(n,step):
+            if n<0:
+                return
+            if n==0:
+                self.res=min(self.res,step)
+                return
+            for i in range(1,n):
+                fun(n-i*i,step+1)
+            return
+
+        fun(n,0)
+        return self.res
+import math
+class Solution:
+    def numSquares(self, n: int) -> int:
+        square_nums=[i**2 for i in range(0,int(math.sqrt(n))+1)]
+        dp=[i for i in range(n+1)]
+        for i in range(n+1):
+            for j in square_nums:
+                if i<j:
+                    break
+                dp[i]=min(dp[i],dp[i-j]+1)
+        return dp[n]
+
+
+a=Solution()
+print(a.numSquares(12))
