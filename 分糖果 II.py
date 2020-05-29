ac9c758d38323665991122a7ae45eab70f8a5987
getabear <1874178998@qq.com>
2020-05-29 10:42:59 +0800

    暴力算法

---
+++ 分糖果 II.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution1:
+    def distributeCandies(self, candies: int, num_people: int) -> List[int]:
+        ret=[0]*num_people
+        candie=1
+        index=0
+        while candies>0:
+            if candie<candies:
+                ret[index]+=candie
+            else:
+                ret[index]+=candies
+            candies-=candie
+            index=(index+1)%num_people
+            candie+=1
+        return ret
+class Solution:
+    def distributeCandies(self, candies: int, num_people: int) -> List[int]:
+
+
+
+a=Solution()
+candies = 10
+num_people = 3
+print(a.distributeCandies(candies,num_people))
