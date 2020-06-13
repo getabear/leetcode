f5428bf0d957b5633bc72b0f7a98b493bc6b2e0c
getabear <1874178998@qq.com>
2020-06-13 08:44:48 +0800

    动态规划

--- 爬楼梯.py
+++ 爬楼梯.py
@@ -1,5 +1,5 @@
 import time
-class Solution:
+class Solution1:
     def climbStairs(self, n: int) -> int:
         dp=[0 for i in range(0,n+2)]
         # print(dp)
@@ -16,7 +16,22 @@ class Solution:
             return dp[i]
         return fun(0,n)
 
-a=Solution()
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp_1=2    #前一步的方法数
+        dp_2=1    #前两步的方法数
+        if n==1:
+            return 1
+        if n==2:
+            return 2
+        start=3
+        while start<=n:
+            tmp=dp_1+dp_2
+            dp_2=dp_1
+            dp_1=tmp
+            start+=1
+        return tmp
+a=Solution1()
 for i in range(1,10):
     print(a.climbStairs(i))
 print(time.process_time())
