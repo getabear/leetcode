8274d632724f32ac12d5f7d8897211bbce1bf284
getabear <1874178998@qq.com>
2020-06-21 10:33:39 +0800

    递归

--- 二叉树中的最大路径和.py
+++ 二叉树中的最大路径和.py
@@ -4,7 +4,7 @@ class TreeNode:
         self.left = None
         self.right = None
 
-class Solution:
+class Solution1:
     def maxPathSum(self, root: TreeNode) -> int:
         self.ret=float("-inf")
         def fun(root: TreeNode):  #递归,返回左右树到上一个节点的最大值
@@ -14,5 +14,18 @@ class Solution:
             right=fun(root.right)
             self.ret=max(left+right+root.val,self.ret)   #记录最大值
             return max(0,left+root.val,right+root.val)  #返回左右树到上一个节点的最大值
+        fun(root)
+        return self.ret
+
+class Solution:
+    def maxPathSum(self, root: TreeNode) -> int:
+        self.ret=float("-inf")
+        def fun(root):
+            if not root:
+                return 0
+            left=fun(root.left)
+            right=fun(root.right)
+            self.ret=max(self.ret,max(left,right,left+right)+root.val,root.val)
+            return max(max(left,right)+root.val,root.val)
         fun(root)
         return self.ret
