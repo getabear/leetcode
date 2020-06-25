7f64ceb3f8c5cfb1c27a6a1b91a2fccdf531278d
getabear <1874178998@qq.com>
2020-06-25 12:52:56 +0800

    添加动态规划的解法

--- 单词拆分.py
+++ 单词拆分.py
@@ -14,7 +14,7 @@ class Solution1:
                 else:
                     continue
         return False
-class Solution:
+class Solution2:
     #优化算法,解决子问题,加了字典用以记录,成功提交通过超越98%的提交
     def __init__(self):
         self.mem={}
@@ -35,7 +35,26 @@ class Solution:
                     continue
         return False
 
-a=Solution1()
-s="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
-wordDict=["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        #使用动态规划
+        #dp数组含义，dp[i]代表从i到s的末尾是否可以拆分
+        dp=[False]*(len(s)+1)
+        #初始状态：
+        dp[len(s)]=True
+        #状态转移  dp[i]=s[i:i+len(word)]==word and dp[i+len(word)]
+        for i in range(len(s)-1,-1,-1):
+            for word in wordDict:
+                n=len(word)
+                if i+n>len(s):
+                    dp[i]=False
+                else:
+                    dp[i]=s[i:i+n]==word and dp[i+n]
+                    if dp[i]:
+                        break
+        return dp[0]
+
+a=Solution()
+s="leetcode"
+wordDict=["leet","code"]
 print(a.wordBreak(s,wordDict))
