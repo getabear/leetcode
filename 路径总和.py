12012bac09d9e9fb308e003dcff8668db9c0eefd
getabear <1874178998@qq.com>
2020-07-07 17:31:41 +0800

    dfs

--- 路径总和.py
+++ 路径总和.py
@@ -1,12 +1,10 @@
-# import git
-
 class TreeNode:
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
 
-class Solution:
+class Solution1:
     def hasPathSum(self, root, sum):
         if not root:
             return False
@@ -16,8 +14,26 @@ class Solution:
         return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
 
 
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        def fun(root,val):
+            if not root:
+                return False
+            # 判断是否到达叶子节点
+            val+=root.val
+            if not root.left and not root.right:
+                return val==sum
+            if(fun(root.left,val)):
+                return True
+            return fun(root.right,val)
+        return fun(root,0)
+
+
+
+
 
 #
-# node=creat_er([5,4,8,11,None,13,4,7,2,None,None,None,1])
-# a=Solution()
-# print(a.hasPathSum(node,22))
+from git import creat_er
+node=creat_er([5,4,8,11,None,13,4,7,2,None,None,None,1])
+a=Solution()
+print(a.hasPathSum(node,22))
