c372ad53236d24c66843c2d3ea4f5f0050f50cb0
getabear <1874178998@qq.com>
2020-07-15 09:09:45 +0800

    双指针

---
+++ 392. 判断子序列.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        idx1,idx2=0,0
+        while idx1<len(s) and idx2<len(t):
+            if s[idx1]==t[idx2]:
+                idx1+=1
+                idx2+=1
+            else:
+                idx2+=1
+        return idx1==len(s)
+
+a=Solution()
+s = "abc"
+t = "ahbgdc"
+print(a.isSubsequence(s,t))
